f7c82bed6aeacabce9043691daf643da16a030a2
Zandy <160404236+ZapGaming@users.noreply.github.com>
2026-03-24 16:34:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const axios = require('axios');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import express from 'express';
+import axios from 'axios';
+import mongoose from 'mongoose';
+import cors from 'cors';;
 
 const app = express();
 app.use(cors());
